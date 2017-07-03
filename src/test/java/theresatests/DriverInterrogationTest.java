033e504c56148bbee5fbcef8c473e24726af442a
Theresa Neate <theresa.neate@rea-group.com>
2017-07-03 15:48:13 +1000

    Updated POM

--- src/test/java/theresatests/DriverInterrogationTest.java
+++ src/test/java/theresatests/DriverInterrogationTest.java
@@ -1,16 +1,28 @@
 package theresatests;
 
 import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
 
 
 /**
  * Created by theresa.neate on 3/07/2017.
  */
+
 public class DriverInterrogationTest {
+
+    static ChromeDriver driver = new ChromeDriver() ;
+    String title;
 
     @BeforeClass
     public static void setupClass(){
         System.out.println("Before the class");
+        driver.navigate().to("http://theresaneate.com");
     }
 
     @Before
@@ -19,22 +31,19 @@ public class DriverInterrogationTest {
     }
 
     @Test
-    public void myTrueTest(){
-        Assert.assertTrue(true);
+    public void titleTest(){
+        title = driver.getTitle();
+        System.out.println("The driver title is: " + title);
     }
 
-    @Test
-    public void myFalseTest(){
-        Assert.assertFalse(false);
 
+    @After
+    public void afterTest(){
+        System.out.println("After test"  + "\n");
+    }
 
-        @After
-        public void afterTest(){
-            System.out.println("After test"  + "\n");
-        }
-
-        @AfterClass
-        public static void cleanUp(){
-            System.out.println("After class cleanup");
-        }
+    @AfterClass
+    public static void cleanUp(){
+        System.out.println("After class cleanup");
+    }
 }
